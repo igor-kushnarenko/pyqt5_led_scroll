18e50d45384dde1a0935021ff6b0ac96eb49e335
max <anapa.igor@yandex.ru>
2022-05-24 10:28:16 +0300

    - вывод строки собранной из списка;

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QPushButton
 from PyQt5.QtGui import QPainter, QColor, QPen, QBrush, QFont
 from PyQt5.QtCore import Qt, QTimer
 
-message = "Добро пожаловать в отель Довиль! Рады видеть вас на наших мероприятиях. Сегодня вечером!" \
-          "Добро пожаловать в отель Довиль! Рады видеть вас на наших мероприятиях. Сегодня вечером! NHEEEE"
+list_message = ['Добро пожаловать в Отель Довиль!', 'Сегодня вторник, 24 мая.',
+           'В СВЯЗИ С ПОГОДНЫМИ УСЛОВИЯМИ ПРОВЕДЕНИЕ ВЕЧЕРНИХ МЕРОПРИЯТИЙ ПЕРЕНОСИТСЯ НА ТЕРРАСУ МАРИНИ.']
+message = ' '.join(list_message)
 
 
 class myApp(QWidget):
@@ -16,38 +17,38 @@ class myApp(QWidget):
         self.init_ui()
 
     def init_ui(self):
-        self.speed = 25
+        self.SPEED = 5
         self.timer = QTimer(self)
+        self.X = 800
+        self.Y = 50
+        self.FONT_SIZE = 25
 
-        self.x = 800
-        self.y = 50
-        self.setWindowTitle("Наши новости")
+        self.setWindowTitle('Doville news')
+        self.setGeometry(100, 100, 750, 100)
 
         self.label = QLabel(message, self)
-
-        self.label.move(self.x, self.y)
-        self.label.setFont(QFont('Arial', 20))
-        self.setGeometry(100, 100, 750, 100)
+        self.label.move(self.X, self.Y)
+        self.label.setFont(QFont('Arial', self.FONT_SIZE))
 
         if self.timer.isActive():
             self.timer.stop()
             self.timer.timeout.disconnect()
-            self.timer.start(self.speed)
+            self.timer.start(self.SPEED)
             self.timer.timeout.connect(self.move_label_left)
         else:
-            self.timer.start(self.speed)
+            self.timer.start(self.SPEED)
             self.timer.timeout.connect(self.move_label_left)
 
         self.show()
 
     def move_label_left(self):
-        if self.x == -(5 * len(message) + 800):
-            self.x = 800
-            self.x = self.x - 1
-            self.label.move(self.x, self.y)
+        if self.X == -(17 * len(message) + 800):
+            self.X = 800
+            self.X = self.X - 1
+            self.label.move(self.X, self.Y)
         else:
-            self.x = self.x - 1
-            self.label.move(self.x, self.y)
+            self.X = self.X - 1
+            self.label.move(self.X, self.Y)
 
 
 if __name__ == '__main__':
