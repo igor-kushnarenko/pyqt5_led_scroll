1b9658f44dd89067b8105a4f3877eb2ccc9384a4
max <anapa.igor@yandex.ru>
2022-05-24 09:48:12 +0300

    -without button start;

--- main.py
+++ main.py
@@ -18,31 +18,27 @@ class myApp(QWidget):
     def init_ui(self):
         self.speed = 25
         self.timer = QTimer(self)
-        btn_start = QPushButton("start", self)
-        btn_start.move(10, 10)
+
         self.x = 800
         self.y = 50
         self.setWindowTitle("Наши новости")
+
         self.label = QLabel(message, self)
+
         self.label.move(self.x, self.y)
         self.label.setFont(QFont('Arial', 20))
         self.setGeometry(100, 100, 750, 100)
 
-        btn_start.clicked.connect(self.buttonClicked)
+        if self.timer.isActive():
+            self.timer.stop()
+            self.timer.timeout.disconnect()
+            self.timer.start(self.speed)
+            self.timer.timeout.connect(self.move_label_left)
+        else:
+            self.timer.start(self.speed)
+            self.timer.timeout.connect(self.move_label_left)
 
         self.show()
-
-    def buttonClicked(self):
-        sender = self.sender()
-        if sender.text() == "start":
-            if self.timer.isActive():
-                self.timer.stop()
-                self.timer.timeout.disconnect()
-                self.timer.start(self.speed)
-                self.timer.timeout.connect(self.move_label_left)
-            else:
-                self.timer.start(self.speed)
-                self.timer.timeout.connect(self.move_label_left)
 
     def move_label_left(self):
         if self.x == -(5 * len(message) + 800):
