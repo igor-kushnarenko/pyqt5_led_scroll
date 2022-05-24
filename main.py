fdabc4143179a81cf20828646a6a395e79680fb2
max <anapa.igor@yandex.ru>
2022-05-24 13:13:53 +0300

    - добавил окно для объявлений, убрал шапки в окнах;

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import sys
 from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QMainWindow
 from PyQt5.QtGui import QFont
-from PyQt5.QtCore import QTimer
+from PyQt5.QtCore import QTimer, Qt
 
-from parser import message
+from parser import message, ads
 
 
 class myApp(QWidget):
-    def __init__(self):
+    def __init__(self, message, speed, start_point):
         QMainWindow.__init__(self)
+        self.message = message
+        self.SPEED = speed
+        self.start_point = start_point
         self.init_ui()
 
     def init_ui(self):
-        self.SPEED = 10
         self.timer = QTimer(self)
         self.X = 1500
         self.Y = 50
         self.FONT_SIZE = 25
 
         self.setWindowTitle('Doville news')
-        self.setGeometry(100, 100, self.X, self.Y)
+        self.setGeometry(0, self.start_point, self.X, self.Y)
         self.setStyleSheet("background-color: black")
+        self.setWindowFlags(Qt.FramelessWindowHint) # убрать шапку
 
-        self.label = QLabel(message, self)
+        self.label = QLabel(self.message, self)
         self.label.setStyleSheet('color: white')
         self.label_x = self.X - 50
         self.lable_y = 8
@@ -41,7 +44,7 @@ class myApp(QWidget):
         self.show()
 
     def move_label_left(self):
-        if self.label_x == -(15 * len(message) + self.X):
+        if self.label_x == -(15 * len(self.message) + self.X):
             self.label_x = self.X - 50
             self.label_x = self.label_x - 1
             self.label.move(self.label_x, self.lable_y)
@@ -52,5 +55,6 @@ class myApp(QWidget):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = myApp()
+    ex = myApp(message, speed=10, start_point=0)
+    ads = myApp(ads, speed=15, start_point=75)
     sys.exit(app.exec_())
